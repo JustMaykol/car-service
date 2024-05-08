6ad897f0fb36632fe2c094e361b000a05f876060
Maykol Morales Morante <maykol.morales.morante@gmail.com>
2024-05-07 22:22:36 -0500

    Stable

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from uuid import uuid4
-from bcrypt import gensalt, hashpw, checkpw
 
 from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
@@ -7,94 +6,100 @@ from flask_pymongo import PyMongo
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'secret'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/users'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/cars'
 
 mongo = PyMongo(app)
 
-db = mongo.db.current
+db = mongo.db.dev
 
 
-# TODO: expand user schema to include email, phone, etc.
+# car crud
 
+@app.route('/car', methods=['POST'])
+def create_car():
+    brand = request.json['brand']
+    model = request.json['model']
+    color = request.json['color']
 
-@app.route('/login', methods=['POST'])
-def login():
-    name = request.json['name']
-    password = request.json['password']
+    year = request.json['year']
+    type = request.json['type']
 
-    user = db.find_one({'name': name})
+    image = request.json['image']
 
-    if not user:
-        return jsonify({'message': 'invalid credentials'}), 401
-
-    if not checkpw(password.encode('utf-8'), user['password']):
-        return jsonify({'message': 'invalid credentials'}), 401
-
-    return jsonify({'user': user['id']}), 200
-
-
-# user crud
-
-@app.route('/user', methods=['POST'])
-def create_user():
-    name = request.json['name']
-    password = request.json['password']
-
-    if db.find_one({'name': name}):
-        return jsonify({'message': f'{name} already exists'}), 401
+    if not brand or not model or not color or not year or not type or not image:
+        return jsonify({'message': 'missing data'}), 400
 
     db.insert_one({
-        'id': str(uuid4()),
-        'name': name,
-        'password': hashpw(password.encode('utf-8'), gensalt(14))
+        '_id': str(uuid4()),
+
+        'brand': brand,
+        'model': model,
+        'color': color,
+
+        'year': year,
+        'type': type,
+
+        'image': image
     })
 
-    return jsonify({'message': f'{name} created'}), 200
+    return jsonify({'message': 'created'}), 200
 
 
-@app.route('/user/<id>', methods=['GET'])
-def read_user(id):
-    user = db.find_one({'id': id})
+@app.route('/car/<_id>', methods=['GET'])
+def read_car(_id):
+    car = db.find_one({'_id': _id})
 
-    if not user:
-        return jsonify({'message': 'user not found'}), 404
+    if not car:
+        return jsonify({'message': 'car not found'}), 404
 
-    user.pop('_id')
-    user.pop('password')
-
-    return jsonify(user), 200
+    return jsonify(car), 200
 
 
-@app.route('/user/<id>', methods=['PUT'])
-def update_user(id):
-    name = request.json['name']
-    password = request.json['password']
+@app.route('/car/<_id>', methods=['PUT'])
+def update_car(_id):
+    brand = request.json['brand']
+    model = request.json['model']
+    color = request.json['color']
 
-    user = db.find_one({'id': id})
+    year = request.json['year']
+    type = request.json['type']
 
-    if not user:
-        return jsonify({'message': 'user not found'}), 404
+    image = request.json['image']
 
-    db.update_one({'id': id}, {
+    if not brand or not model or not color or not year or not type or not image:
+        return jsonify({'message': 'missing data'}), 400
+
+    car = db.find_one({'_id': _id})
+
+    if not car:
+        return jsonify({'message': 'car not found'}), 404
+
+    db.update_one({'_id': _id}, {
         '$set': {
-            'name': name,
-            'password': hashpw(password.encode('utf-8'), gensalt(14))
+            'brand': brand,
+            'model': model,
+            'color': color,
+
+            'year': year,
+            'type': type,
+
+            'image': image
         }
     })
 
-    return jsonify({'message': f'{name} updated'}), 200
+    return jsonify({'message': 'updated'}), 200
 
 
-@app.route('/user/<id>', methods=['DELETE'])
-def delete_user(id):
-    user = db.find_one({'id': id})
+@app.route('/car/<_id>', methods=['DELETE'])
+def delete_car(_id):
+    car = db.find_one({'_id': _id})
 
-    if not user:
-        return jsonify({'message': 'user not found'}), 404
+    if not car:
+        return jsonify({'message': 'car not found'}), 404
 
-    db.delete_one({'id': id})
+    db.delete_one({'_id': _id})
 
-    return jsonify({'message': 'user deleted'}), 200
+    return jsonify({'message': 'deleted'}), 200
 
 
 if __name__ == '__main__':
